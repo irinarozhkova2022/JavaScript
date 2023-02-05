5b56bed89435def54158bb9557de1d50c65d2a4e
Irina <irina-rozhkova-87@mail.ru>
2023-02-05 04:04:57 +0300

    Initial commit

---
+++ seminar2/script.js
@@ -0,0 +1,69 @@
+//Семинар2
+// let age = ("Ваш возраст?", 18);
+// console.log("Вам ${age} лет");
+
+// let name = ("Ваше имя?", "Юлия");
+// console.log(`Добро пожаловать на сайт, ${name}`);
+        
+// let a = 13;
+// let b = 5;
+// console.log(a % b);
+
+// let a;
+// console.log(alert(a));
+
+// a = ;
+// b = ;
+// с = ;
+// console.log
+
+// let a = "2";
+// let b = "3";
+// console.log(a+b);
+
+// let a = parseInt(prompt("Введите первое число: ", 0)); //Number вместо ParseInt. разницы не будет, но если требуется найти остаток от деления лучше парс использовать
+// let b = parseInt(prompt("Введите второе число: ", 0)); //Number вместо ParseInt
+// let sum = a+b;
+// let dif = a-b;
+// let mult = a*b;
+// let div = a/b;
+// let rest = a%b;
+// console.log(sum, dif, mult, div, rest);
+
+// console.log(String(true));
+
+// console.log("a" + true);
+
+// console.log(Number(true));
+
+// console.log(true + 1);
+
+// console.log(true + true);
+
+// console.log(true - true);
+
+// console.log(String(true) + Number(true));
+
+
+// let a = Number(prompt("Введите число"));
+// if (a > 5) {
+//     console.log("Число больше 5");
+// } else if (a < 5) {
+//     console.log("Число меньше 5");
+// } else {
+//     console.log("Число равно 5");
+// }
+
+// let test1 = Number(prompt("Введите первое число: "));
+// let test2 = Number(prompt("Введите второе число: "));
+// (test1 === test2) ? console.log("числа равны") : console.log("числа не равны");
+
+// let test1 = Number(prompt("Введите первое число: "));
+// if (test1 > 0 && test1 < 15) {
+//     console.log("Переменная прошла проверку");
+// } else {
+//     console.log("Переменная не прошла проверку");
+// } 
+
+// (test1 > 0 && test1 < 15) ? console.log("Переменная прошла проверку") : console.log("Переменная не прошла проверку");
+
