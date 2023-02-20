c10f60b300db03492ce42951391a1c1ca9c2507b
Irina <irina-rozhkova-87@mail.ru>
2023-02-21 00:29:40 +0300

    initial commit

--- проект/script.js
+++ проект/script.js
@@ -1,10 +1,10 @@
 const data = JSON.parse(dataProducts);
 
 
-const contentBox = document.querySelector('.contentBox');
+const contentBox = document.querySelector('.products');
 
 data.forEach(element => {
-    const products = document.createElement('div');
+    const box = document.createElement('div');
     const img = document.createElement('img');
     const good = document.createElement('div');
     const goodName = document.createElement('h3');
@@ -16,9 +16,9 @@ data.forEach(element => {
     goodText.textContent = element.goodText;
     goodPrice.textContent = element.goodPrice;
 
-    contentBox.appendChild(products);
-    products.appendChild(img);
-    products.appendChild(good);
+    contentBox.appendChild(box);
+    box.appendChild(img);
+    box.appendChild(good);
     good.appendChild(goodName);
     good.appendChild(goodText);
     good.appendChild(goodPrice);
